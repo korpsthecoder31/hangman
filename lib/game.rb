8bdf59fbbf213dd9d27b0c792b54ae7246e3a7b3
korpsthecoder31 <korpsthewriter@gmail.com>
2025-04-09 07:55:42 +0800

    Add conditional so get_word returns sample between 5-12 letters

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,12 @@ class Game
 
   def get_word
     word_file = File.open("google-10000-english-no-swears.txt")
-    File.readlines(word_file).sample
+    loop do
+      word = File.readlines(word_file).sample
+      if word.length >= 5 && word.length <=12
+        return word
+      end
+    end
   end
 
   def play_game
