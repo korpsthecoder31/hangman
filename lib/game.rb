e68ceaaebf11429bd4034e853d3800b27ff88c35
korpsthecoder31 <korpsthewriter@gmail.com>
2025-04-09 08:23:19 +0800

    Add checks to player_guess to ensure guess is a valid letter

--- lib/game.rb
+++ lib/game.rb
@@ -4,16 +4,30 @@ class Game
   end
 
   def get_word
-    word_file = File.open("google-10000-english-no-swears.txt")
+    words = File.open("google-10000-english-no-swears.txt").map(&:strip)
     loop do
-      word = File.readlines(word_file).sample
-      if word.length >= 5 && word.length <=12
-        return word
-      end
+      word = words.sample.upcase
+      return word if word.length >= 5 && word.length <=12
     end
   end
 
   def play_game
     puts @word
+    loop do
+      player_guess
+    end
+  end
+
+  def player_guess
+    puts "Please select letter:"
+    guess = gets.chomp.upcase
+    loop do
+      if guess.length == 1 && (guess >= 'A' && guess <= 'Z')
+        return guess
+      else
+        puts "Selection is not a valid letter. Try again."
+        guess = gets.chomp.upcase  
+      end 
+    end
   end
 end
